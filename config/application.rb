2613822ce6d9ae3c1374790e8030199486ca7585
Ollie Tiu <ollielt@yahoo.com>
2013-10-22 18:28:42 -0700

    Finish layout and routes

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,10 @@ require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+
+
+
+
 Bundler.require(:default, Rails.env)
 
 module SampleApp
@@ -24,5 +28,7 @@ module SampleApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
   end
 end
