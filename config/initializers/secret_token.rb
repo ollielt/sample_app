3ef657c68f567cc816e9b67de67635d093a82886
Ollie Tiu <ollielt@yahoo.com>
2013-11-04 12:09:18 -0800

    add new file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,20 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = '9f3b9fbfb050c4de9dbfe9b69d4b030c4e69ea477de0d2363e090a3f2038123da0235933c2d7c1785efce3ae5744b8fc1b6c4f0548c149099f531114979e5d6a'
+require 'securerandom'
+
+def secure_token
+	token_file = Rails.root.join('.secret')
+	if File.exists?(token_file)
+	
+		# Use the existing token.
+		File.read(token_file).chomp
+	else
+		# Generate a new token and store it in token_file
+		token = SecureRandom.hex(64)
+		File.write(token_file, token)
+		token
+	end
+end
+
+Wandaing::Application.config.secret_key_base = secure_token
